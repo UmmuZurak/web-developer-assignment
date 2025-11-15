01eedb81ae6b2e27d1d141d63c710ff6cefac4c8
UmmuZurak <51839140+UmmuZurak@users.noreply.github.com>
2025-11-15 09:57:20 +0000

    update loader component

--- frontend/src/components/ui/loader.tsx
+++ frontend/src/components/ui/loader.tsx
@@ -1,6 +1,6 @@
 export default function Loader() {
   return (
-    <div className="w-full h-full flex items-center justify-center min-h-[220px] border  rounded-lg">
+    <div className="w-full h-full flex items-center justify-center min-h-[220px] border rounded-lg">
       <div className="lds-ellipsis">
         <div></div>
         <div></div>
