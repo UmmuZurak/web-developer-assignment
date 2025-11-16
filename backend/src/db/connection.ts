0b6647a3977e885a90bc4b5d655101017826187b
UmmuZurak <51839140+UmmuZurak@users.noreply.github.com>
2025-11-16 17:17:56 +0000

    updates

--- backend/src/db/connection.ts
+++ backend/src/db/connection.ts
@@ -1,6 +1,6 @@
 import Database from "better-sqlite3";
 import path from "path";
-
+process.env.SUPPRESS_NO_CONFIG_WARNING = 'true';
 const dbPath = path.join(process.cwd(), 'data.db');
 
 export const connection = new Database(dbPath, { 
