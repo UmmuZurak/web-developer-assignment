2a583072bfe5a0c1271883a0f716f8d033599cd1
UmmuZurak <51839140+UmmuZurak@users.noreply.github.com>
2025-11-16 17:00:41 +0000

    connection update

--- backend/src/db/connection.ts
+++ backend/src/db/connection.ts
@@ -1,5 +1,9 @@
-import sqlite3 from "better-sqlite3";
-import config from "config";
+import Database from "better-sqlite3";
+import path from "path";
 
-const dbPath = config.get("dbPath") as string;
-export const connection = new sqlite3(dbPath);
+const dbPath = path.join(process.cwd(), 'data.db');
+
+export const connection = new Database(dbPath, { 
+  readonly: true,
+  fileMustExist: true 
+});
